1a7af9d1dd42728b1ee2d17ee73563379d0e0d43
Luisa Kamm <Luisa@Luisas-iMac.fritz.box>
2019-04-06 15:32:31 +0200

    Initial import

---
+++ content/_index.md
@@ -0,0 +1,11 @@
+---
+menu:
+    main_left:
+        pre: '<span class="la la-home"></span>'
+        name: ''
+---
+# Aktuelles
+{{< news-gallery >}}
+{{< facebook "https://www.facebook.com/TTCBuehl/posts/634019940385309" >}}
+{{< facebook "https://www.facebook.com/TTCBuehl/posts/2837819279776628" >}}
+{{< /news-gallery >}}
---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+echo $GITHUB_AUTH_SECRET > ~/.git-credentials && chmod 0600 ~/.git-credentials
+git config --global credential.helper store
+git config --global user.email "ttcbot@users.noreply.github.com"
+git config --global user.name "TTC Bot"
+git config --global push.default simple
+
+rm -rf deployment
+git clone -b master https://github.com/ttc-buehl/ttc-buehl.github.io deployment
+git checkout master
+rsync -av --delete --exclude ".git" public/ deployment
+cd deployment
+git add -add
+
+git commit -m "rebuilding site on `date`, commit ${TRAVIS_COMMIT} and job ${TRAVIS_JOB_NUMBER}" || true
+git push origin master
+
+cd ..
+rm -rf deployment
